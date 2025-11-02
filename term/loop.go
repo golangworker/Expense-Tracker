c6f39eceba9f01dc03e963cc26a667fc73c741c9
Vladimir Aristarkhov <golangworker@gmail.com>
2025-11-02 14:39:10 +0300

    finished tui

--- term/loop.go
+++ term/loop.go
@@ -6,7 +6,7 @@ import (
 	"strconv"
 )
 
-var ErrUseHelp error = fmt.Errorf("use --help or -h to show commands")
+var ErrUseHelp error = fmt.Errorf("use \033[1m--help or -h\033[0m to show commands")
 
 func RunningLoop(args []string) error {
 	if len(args) < 2 {
@@ -59,15 +59,29 @@ func RunningLoop(args []string) error {
 }
 
 func help() {
-	fmt.Println("Available commands:")
-	fmt.Println("  --help, -h")
-	fmt.Println("  --add, -a")
-	fmt.Println("  --delete, -d")
-	fmt.Println("  --edit, -e")
-	fmt.Println("  --summary, -s")
-	fmt.Println("  --month_summary, -ms")
+	fmt.Printf("Available commands:\n\n")
+	fmt.Printf("• \033[1m--help, -h\033[0m info about program\n\n")
+	fmt.Printf("Example: app --add \"Groceries\" 50\n\n")
+
+	fmt.Printf("• \033[1m--add, -a\033[0m (description string, amount float) create new expense\n\n")
+	fmt.Printf("Example: app --add \"Groceries\" 50\n\n")
+
+	fmt.Printf("• \033[1m--delete, -d\033[0m (id int) delete expense\n\n")
+	fmt.Printf("Example: app --delete 1\n\n")
+
+	fmt.Printf("• \033[1m--edit, -e\033[0m (id int, amount float) edit expense\n\n")
+	fmt.Printf("Example: app --edit 1 50\n\n")
+
+	fmt.Printf("• \033[1m--summary, -s\033[0m show list of expenses\n\n")
+	fmt.Printf("Example: app --summary\n\n")
+
+	fmt.Printf("• \033[1m--month_summary, -ms\033[0m (month string) show list of expenses by month\n\n")
+	fmt.Printf("Example: app --month_summary \"January\"\n\n\n")
+
+	fmt.Printf("Last version: \033[1m2.11.2025 github.com/golangworker/Expense-Tracker\033[0m\n\n")
 }
 
+// функция для добавления расхода
 func add(description string, amount float64) error {
 	expence := logic.InitExpence(description, amount)
 	list := logic.InitList()
@@ -81,6 +95,7 @@ func add(description string, amount float64) error {
 	return nil
 }
 
+// функция для удаления расхода
 func delete(id int) error {
 	list := logic.InitList()
 	list.LoadFromFile()
@@ -93,11 +108,39 @@ func delete(id int) error {
 	return nil
 }
 
+// функция для редактирования расхода
+func edit(id int, amount float64) error {
+	list := logic.InitList()
+	if err := list.LoadFromFile(); err != nil {
+		return err
+	}
+	if err := list.Update(id, amount); err != nil {
+		return err
+	}
+	if err := list.SaveToFile(); err != nil {
+		return err
+	}
+	return nil
+}
+
+// функция для просмотра расходов
 func summary() error {
 	list := logic.InitList()
 	if err := list.LoadFromFile(); err != nil {
 		return err
 	}
 	list.ShowAllExpenses()
+	return nil
+}
+
+// функция для просмотра расходов за месяц
+func monthSummary(month string) error {
+	list := logic.InitList()
+	if err := list.LoadFromFile(); err != nil {
+		return err
+	}
+	if err := list.ShowForMonthExpenses(month); err != nil {
+		return err
+	}
 	return nil
 }
